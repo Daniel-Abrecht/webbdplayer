313b8ff9d2c533e72f686b878f732568d0365bc5
Daniel Abrecht <public@danielabrecht.ch>
2024-07-16 20:19:13 +0000

    Refactoring

--- www/module/bdplayer.js
+++ www/module/bdplayer.js
@@ -2,9 +2,8 @@
 import { Bluray } from "./bd.mjs";
 import { FileSystem } from "./fs.mjs";
 import { AsyncCreation } from "./utils.mjs";
-import * as SRROFS from "./fs-static-remote-ro.mjs";
 
-class GLBDPlayer extends AsyncCreation {
+export class GLBDPlayer extends AsyncCreation {
   #bluray;
   #gl;
   #program = {};
@@ -84,7 +83,6 @@ void main() {
     this.#bluray.keypress_end(GLBDPlayer.#key_mapping[event.keyCode]);
   };
   connectedCallback(){
-    console.log("connectedCallback")
     //this.#bluray.keypress_start()
     this.#gl.canvas.addEventListener("keydown", this.#onkeydown);
     this.#gl.canvas.addEventListener("keyup", this.#onkeyup);
@@ -194,88 +192,3 @@ void main() {
     gl.disable(gl.SCISSOR_TEST);
   }
 }
-
-class UIPlayer extends HTMLElement {
-  #running;
-  #player;
-  #canvas;
-  #root;
-  #gl;
-  static get observedAttributes() {
-    return ["src"];
-  }
-  constructor(){
-    super();
-    this.#canvas = document.createElement("canvas");
-    this.#gl = this.#canvas.getContext("webgl2", {
-      antialias: false,
-      depth: false,
-      powerPreference: "low-power",
-      premultipliedAlpha: false,
-    });
-    this.#canvas.tabIndex = -1;
-    this.#root = this.attachShadow({ mode: "closed" });
-    this.#root.appendChild(this.#canvas);
-    this.#canvas.width = 1920;
-    this.#canvas.height = 1080;
-    this.style.display = 'inline-flex';
-    this.addEventListener("click", ()=>this.play());
-  }
-  connectedCallback(){
-    this.#player?.connectedCallback?.();
-  }
-  disconnectedCallback(){
-    this.#player?.disconnectedCallback?.();
-  }
-  async open(uri){
-    let [_, type, location] = uri.match(/^(bluray:|video:)?([^]*)$/m)
-    type ??= 'video:';
-    switch(type){
-      case 'bluray:': {
-        const bluray_rootfs = await SRROFS.Directory.create(location);
-        this.#player = await GLBDPlayer.create(bluray_rootfs, {gl:this.#gl});
-        if(this.parentNode)
-          this.#player?.connectedCallback?.();
-      }; break;
-      case 'video:': {
-        throw new Error("Not yet implemented");
-      }; break;
-    }
-    this.main_loop();
-  }
-  async play(){
-    await this.#player.play();
-  }
-  async main_loop(){
-    const NF = { then: c=>requestAnimationFrame(c) };
-    try {
-      if(this.#running)
-        return;
-      this.#running = true;
-      while(this.#running)
-        this.drawFrame(await NF);
-    } finally {
-      this.#running = false;
-    }
-  }
-  drawFrame(){
-    this.#player?.drawFrame?.();
-  }
-  close(){
-    this.#running = false;
-    if(this.parentNode)
-      this.#player?.disconnectedCallback?.();
-    this.#player = null;
-  }
-  attributeChangedCallback(name, oldValue, newValue) {
-    if(oldValue == newValue) return;
-    if(name === 'src'){
-      if(newValue){
-        this.open(newValue);
-      }else{
-        this.close();
-      }
-    }
-  }
-};
-customElements.define("ui-player", UIPlayer);
